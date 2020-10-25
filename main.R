526676c5db906c50b79b9f793afa4b30b5de29f2
adame111 <adam11441144@gmail.com>
2020-10-25 12:01:36 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,22 +1,23 @@
-
-
-unit<-function(index) {
-index.wyniki<-matrix(data=0,nrow=nrow(index),ncol=1)
+unit<-function(index)  {
+index.uz<-matrix(data=0,nrow=nrow(index),ncol=1)
 index.unit<-matrix(data=0,nrow=nrow(index), ncol=ncol(index))  
-for (i in 1:ncol(index)) {
-for (n in 1:nrow(index)){
-if(cor(index[,i],index[,1])<0) {
-   index.unit[n,i]<-(max(index[,i])-index[n,i])/(max(index[,i])-min(index[,i]))
- } 
+for (i in 1:ncol(index))   {
+for (n in 1:nrow(index))        {
+if(cor(index[,i],index[,1])<0)       {
+   index.unit[n,i]<-(max(index[,i])-as.numeric(index[n,i]))/(max(index[,i])-min(index[,i]))
+                                       }                         
   else {
-   index.unit[n,i]<-(index[n,i]-min(index[,i]))/(max(index[,i])-min(index[,i]))
-  }
-}
-}
+   index.unit[n,i]<-(as.numeric(index[n,i])-min(index[,i]))/(max(index[,i])-min(index[,i]))
+        }
+                                 }
+                          }
 
 for(n in 1:nrow(index.unit)){
   index.uz[n,1]<-mean(index.unit[n,])
 }
-}
+return(index.uz)
+                        }
+
+
 unit(index)
-index.uz
+
